9a6cf48251e4df8b8918b860ef44e336b339e6e9
Vladimir Gaskov <vladimir.gaskov@gmail.com>
2017-06-24 01:31:38 +0600

    Added exiting by typing "quit" from keyboard + catching exceptions

--- src/main/java/com/triangle/testtask/Main.java
+++ src/main/java/com/triangle/testtask/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		try {
 			final double sideA = getDoubleFromKeyboard("Please insert sideA:");		
 			final double sideB = getDoubleFromKeyboard("Please insert sideB:");
 			final double sideC = getDoubleFromKeyboard("Please insert sideC:");
 			
 			System.out.println("Testing a triangle with the following side lengths: " + sideA + " " + sideB + " " + sideC);
+
 			switch (Triangle.classify(sideA, sideB, sideC)) {
 			case EQUILATERAL:
 				System.out.println("The triangle is EQUILATERAL");
@@ -26,18 +27,24 @@ public class Main {
 				System.out.println("The given input data does not correspond to any triangle");
 				break;
 			}
-		} catch (IllegalArgumentException e) {
+		} catch (TriangleException e) {
 			System.out.println(e.getLocalizedMessage().toString());
+		} catch (Exception e) {
+			System.out.println("ERROR OCCURED: " + e.getLocalizedMessage().toString());
 		}
 	}
 	
-	private static double getDoubleFromKeyboard(String message) throws IllegalArgumentException {    	
-		System.out.println(message);
-		try (Scanner sc = new Scanner(new InputStreamDecorator(System.in))) {
-			if (sc.hasNextDouble()) {
-				return sc.nextDouble();
-			} else {
-				throw new IllegalArgumentException("the given value is non-numeric");
+	private static double getDoubleFromKeyboard(String message) throws TriangleException {
+		while (true) {
+			System.out.println(message);
+			try (Scanner sc = new Scanner(new InputStreamDecorator(System.in))) {
+				if (sc.hasNext("quit")) {
+					throw new TriangleException("Exiting the program");
+				} else if (sc.hasNextDouble()) {
+					return sc.nextDouble();
+				} else {
+					System.out.println("the given value is non-numeric");
+				}
 			}
 		}
     }
